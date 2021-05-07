9f06fc43f9612125fc352ab8eac04a3fc1dbd930
berezka-creator <berezina.y.v.2015@gmail.com>
2021-05-07 05:22:55 -0700

    Made API call for main video

--- youtubeQuiz/script.js
+++ youtubeQuiz/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
     var key = 'AIzaSyBBF7_kHouQt-er1grk-v4t4iU9YbFdaY8';
     var playlistId = 'PLcHIy7MhWjmgA2YfyLc9kQpYOENBB9X_y';
-    var URL = 'https://www.googleapis.com/youtube/v3/playlists';
-console.log(key)
+    var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+
     var options = {
         part: 'snippet',
         key: key,
@@ -14,7 +14,35 @@ console.log(key)
 
     function loadVids(){
         $.getJSON(URL, options, function(data){
-            console.log(data)
+            console.log(data);
+            var id = data.items[0].snippet.resourceId.videoId; 
+            
+
+            mainVid(id);
         }) 
+    }
+   
+    function mainVid() {
+        
+        $('#video').html(`
+        <iframe width="560" height="315" 
+        src="https://www.youtube.com/embed/"+id title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  
+        `);
+    }
+
+    function resultsLoop(data) {
+
+        var thumb = data.items[0].snippet.thumbnails.medium.url;
+        $('main').html(`
+        <article>
+                <img src="${thumb}"
+                alt=""  class="thumb" />
+                <div class="details">
+                    <h4>Title</h4>
+                    <p>I am a description</p>
+                </div>
+                </article>
+        `)
+
     }
 })
