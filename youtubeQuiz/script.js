854f9494139bdfb6aae85533563e2b193b3e62d6
berezka-creator <berezina.y.v.2015@gmail.com>
2021-05-07 04:36:12 -0700

    Fixed Jqury json link

--- youtubeQuiz/script.js
+++ youtubeQuiz/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var key = 'AIzaSyBBF7_kHouQt-er1grk-v4t4iU9YbFdaY8';
     var playlistId = 'PLcHIy7MhWjmgA2YfyLc9kQpYOENBB9X_y';
     var URL = 'https://www.googleapis.com/youtube/v3/playlists';
-
+console.log(key)
     var options = {
         part: 'snippet',
         key: key,
