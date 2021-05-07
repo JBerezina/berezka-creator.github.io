cf289f8e070c88f460b09c4d494a817ac7d97dd1
berezka-creator <berezina.y.v.2015@gmail.com>
2021-05-07 03:59:26 -0700

    Youtube Quiz API call

---
+++ youtubeQuiz/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    var key = 'AIzaSyBBF7_kHouQt-er1grk-v4t4iU9YbFdaY8';
+    var playlistId = 'PLcHIy7MhWjmgA2YfyLc9kQpYOENBB9X_y';
+    var URL = 'https://www.googleapis.com/youtube/v3/playlists';
+
+    var options = {
+        part: 'snippet',
+        key: 'AIzaSyBBF7_kHouQt-er1grk-v4t4iU9YbFdaY8';
+        maxResults: 20,
+        playlistId: playlistId
+    }
+    loadVids();
+
+    function loadVids(){
+        $.getJSON(URL, options, function(data){
+            console.log(data)
+        })
+        
+    }
+
+
+})
