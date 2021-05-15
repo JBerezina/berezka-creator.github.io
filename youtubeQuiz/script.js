f8a8dff862e4cd82743439994290097efbd62386
berezka-creator <berezina.y.v.2015@gmail.com>
2021-05-15 02:25:11 -0700

    youtubeQuiz changes

--- youtubeQuiz/script.js
+++ youtubeQuiz/script.js
@@ -16,33 +16,44 @@ $(document).ready(function(){
         $.getJSON(URL, options, function(data){
             console.log(data);
             var id = data.items[0].snippet.resourceId.videoId; 
-            
-
+        
             mainVid(id);
+            resultsLoop(data);
         }) 
     }
    
-    function mainVid() {
+    function mainVid(id) {
         
         $('#video').html(`
         <iframe width="560" height="315" 
-        src="https://www.youtube.com/embed/"+id title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  
+        src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  
         `);
     }
 
     function resultsLoop(data) {
 
-        var thumb = data.items[0].snippet.thumbnails.medium.url;
-        $('main').html(`
-        <article>
-                <img src="${thumb}"
-                alt=""  class="thumb" />
-                <div class="details">
-                    <h4>Title</h4>
-                    <p>I am a description</p>
-                </div>
-                </article>
-        `)
+        $.each(data.items, function(i, item){
 
+            var thumb = item.snippet.thumbnails.medium.url;
+            var title=item.snippet.title;
+            var desc = item.snippet.description.substring(0, 100);
+            var vid=item.snippet.resourceId.videoId;
+            
+            $('main').append(`
+            <article class="item" data-key="${vid}">
+                    <img src="${thumb}"
+                    alt=""  class="thumb" />
+                    <div class="details">
+                        <h4>${title}</h4>
+                        <p>${desc}</p>
+                    </div>
+                    </article>
+            `);
+        })
     }
+
+    $('main').on('click', 'article', function (){
+        var id = $(this).attr('data-key');
+        mainVid(id);
+    });
 })
